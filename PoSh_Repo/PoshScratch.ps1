a2abb8716aadd0b9ac0a21474a21da6580a5f00b
PabloBrewster <39218285+PabloBrewster@users.noreply.github.com>
2022-03-12 08:28:01 +0000

    PoSh Folder Tidy

--- PoSh_Repo/PoshScratch.ps1
+++ PoSh_Repo/PoshScratch.ps1
@@ -1,9 +1,9 @@
-#Install-Module "AZ"
-#Connect-AzAccount -Tenant "de0e09a2-044a-40f8-96a9-77948b05d041"
+I#nstall-Module "AZ"
+Connect-AzAccount -Tenant "de0e09a2-044a-40f8-96a9-77948b05d041"
 
 Get-Module -ListAvailable
 
-#Get-Command -Verb Get -Noun AZ*
-#Get-Command -Module Az.KeyVault
+Get-Command -Verb Get -Noun AZ*
+Get-Command -Module Az.KeyVault
 
-#New-AzKeyVault -VaultName "PabloBrewster" -ResourceGroupName "PabloBrewster" -Location "UK South"
+New-AzKeyVault -VaultName "PabloBrewster" -ResourceGroupName "PabloBrewster" -Location "UK South"
